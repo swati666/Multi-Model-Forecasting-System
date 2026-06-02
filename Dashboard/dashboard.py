e7740c7ba181d147921bd35a921f27ca7a8ba08f
swati666 <138058255+swati666@users.noreply.github.com>
2026-06-02 20:38:40 +0530

    Added Docker deployment and Render configuration

--- Dashboard/dashboard.py
+++ Dashboard/dashboard.py
@@ -51,8 +51,8 @@ best_model_registry, metrics_registry = (
 # FASTAPI URL
 # ==================================================
 
-API_URL = "http://localhost:8080"
-
+# API_URL = "http://localhost:8080"
+API_URL = "https://multi-model-forecasting-system.onrender.com"
 
 # ==================================================
 # HEADER
